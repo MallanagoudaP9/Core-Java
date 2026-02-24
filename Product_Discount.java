cccb48dd54dbfa4ecec921c5f55110a1d469ad0c
MallanagoudaP9 <pmallanagouda40@gmail.com>
2026-02-24 09:43:24 +0530

    Create Product_Discount.java

---
+++ Product_Discount.java
@@ -0,0 +1,23 @@
+package fidelity.programs;
+
+public class Product_Discount {
+
+	public static double getFinalAmount(double amount, boolean isMember) {
+		if(amount >= 1000) {
+			if(isMember) {
+				return amount - (amount * 0.20);
+			}
+			else {
+				return amount - (amount * 0.10);
+			}
+		}
+		else {
+			return amount;
+		}
+	}
+	
+public static void main(String[] args) {
+  System.out.println(getFinalAmount(1500, true));
+
+}
+}
